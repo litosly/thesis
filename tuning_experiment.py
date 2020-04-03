4427c2df4069726b73c3dfafb0e1c2f18d5cfe1f
litosly <litos.li@mail.utoronto.ca>
2020-04-03 15:40:37 -0400

    minor change in data file name2

--- tuning_experiment.py
+++ tuning_experiment.py
@@ -40,7 +40,7 @@ def main(args):
         'rank' : 200 
     }
     
-    keyphrases_names = load_dataframe_csv(path = args.data_dir, name = "Keyphrases.csv")['Phrases'].tolist()
+    keyphrases_names = load_dataframe_csv(path = args.data_dir, name = "KeyPhrases.csv")['Phrases'].tolist()
     keyphrase_selection_method = args.keyphrase_selection_method
     # lambs = [0.0001, 0.001, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 30, 50, 70, 90, 100, 200, 500, 1000, 10000, 100000]
     lambs = [1,10]
